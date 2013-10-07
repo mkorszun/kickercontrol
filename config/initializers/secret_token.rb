846e04cf150caf81e19f47675e047d404867e889
Mateusz Korszun <mkorszun@gmail.com>
2013-10-07 10:43:52 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Kickercontrol::Application.config.secret_key_base = '8e19e89b4051e18a0e41691bf447a107b4d085101a8ddb6589eb3d4031c29277fc834f36fefb6864dfd8d3a48054aea648b83c6c39b993f2a248369c401c625e'
